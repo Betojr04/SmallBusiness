225ca75d77622f7fcb4a4443ce5362f371d4ff34
Betojr04 <betomasia12@gmail.com>
2024-03-14 10:19:18 -0700

    added route to fetch api data from fake store only for route that will be used to display the 3 features products in the featured section of the home page added try except block to handle errors as well

--- server/api/api_routes.py
+++ server/api/api_routes.py
@@ -6,3 +6,17 @@ api = Blueprint('api', __name__)
 @api.route('/')
 def hello_world():
     return jsonify({'message': 'Just to make sure that the backend is working'})
+
+
+@api.route('/featured-products', methods=['GET'])
+def featured_products():
+    try:
+        response = requests.get('https://fakestoreapi.com/products?limit=3')
+        response.raise_for_status()
+        
+        
+        return jsonify(response.json())
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': str(e)}), 500
+    
+    
