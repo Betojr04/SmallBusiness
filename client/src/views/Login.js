6c5cbb0f8d5230639158ed90838a7ac117a14692
Betojr04 <betomasia12@gmail.com>
2024-03-16 10:59:13 -0700

    finished styling a basic login page with its form

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -2,7 +2,38 @@ import React from "react";
 import "../styles/login.css";
 
 const Login = () => {
-  return <main className="login-container">This is the login page</main>;
+  return (
+    <main className="login-container">
+      <form>
+        <label htmlFor="email">Email*</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email Address"
+          required
+        />
+        <label htmlFor="password">Password*</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Your Password"
+          required
+        />
+
+        <button className="submit" type="submit">
+          Login
+        </button>
+      </form>
+      <div className="register-instead">
+        <p>Don't have an account?</p>
+        <button className="user-button">
+          <a href="/create-account">Create Account</a>
+        </button>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
