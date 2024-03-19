a4eeb2b51e7f852a2843364ba39305a386543d6e
Betojr04 <betomasia12@gmail.com>
2024-03-18 22:53:26 -0700

    created a create account route checks for first and last name, email and password hashes password need to add jwt and test still

--- server/api/auth.py
+++ server/api/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from api.models import User, db
 import json
 import requests
 import hashlib
@@ -8,8 +9,36 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/register', methods=['POST'])
 def create_new_user():
-    data = request.get_data()
-    json_data = json_data.get(data)
+    data = request.get_json()
+    
+    first_name = data['first_name']    
+    last_name = data['last_name']
+    email = data['email']
+    password = data['password']
+    
+    if not first_name:
+        return jsonify({"msg": "First name is required"}), 400
+    
+    if not last_name:
+        return jsonify({"msg": "Last name is required"}), 400
+    
+    if not email:
+        return jsonify({"msg": "Email is required"}), 400
+    
+    if not password:
+        return jsonify({"msg": "Password is required"}), 400
+    
+    if User.query.filter_by(email=email).first():
+        return jsonify({"msg": "Email already exists"}), 400
+    
+    password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    
+    user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+    
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify({"msg": "User created successfully"}), 200
     
     
     
