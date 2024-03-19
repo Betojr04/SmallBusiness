8a8e52f6fdba769eaf1bea1d5875c8709746005a
Betojr04 <betomasia12@gmail.com>
2024-03-18 22:49:43 -0700

    craeted user data model for when a user creates an account with us

--- server/api/models.py
+++ server/api/models.py
@@ -1,1 +1,20 @@
 from api import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
+            "username": self.username,
+            "email": self.email
+        }
