001eed29e9c6bce1df3b41e2466962580cade67e
Betojr04 <betomasia12@gmail.com>
2024-03-20 11:08:03 -0700

    added categories fetch useEffect to disaply the list of categories avaialble not styled and do not lead to separate page yet

--- client/src/components/CategoriesSection.js
+++ client/src/components/CategoriesSection.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/categoriesSection.css";
 
 export const CategoriesSection = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/categories")
+      .then((response) => response.json())
+      .then((data) => setCategories(data))
+      .catch((error) => console.log(error)); // Move the catch block here
+  }, []);
   return (
     <section className="categories-section">
       <h2>Categories</h2>
-      <div className="categories-container"></div>
+      <ul className="categories-list">
+        {categories.map((category, index) => (
+          <li key={index}>{category}</li>
+        ))}
+      </ul>
     </section>
   );
 };
