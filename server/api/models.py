7d6e082a5304f53f93d7eb85eba2d96fa273a55a
Betojr04 <betomasia12@gmail.com>
2024-03-19 21:52:26 -0700

    initialized the database sqlite for dev purposes and testing in postman the register new user route added user data model and inlcuded the password field

--- server/api/models.py
+++ server/api/models.py
@@ -4,7 +4,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    # username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
