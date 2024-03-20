fe27017aa90c01fd7accfde91b111065338f2287
Betojr04 <betomasia12@gmail.com>
2024-03-20 10:44:17 -0700

    added basic outline of categories section this is where i will display all the applicable categories from fakestore api

---
+++ client/src/components/CategoriesSection.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/categoriesSection.css";
+
+export const CategoriesSection = () => {
+  return (
+    <section className="categories-section">
+      <h2>Categories</h2>
+      <div className="categories-container"></div>
+    </section>
+  );
+};
--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -3,12 +3,14 @@ import "../styles/landingpage.css";
 import { HeroSection } from "../components/HeroSection";
 import { FeaturedProducts } from "../components/FeaturedProducts";
 import { Testimonials } from "../components/Testimonials";
+import { CategoriesSection } from "../components/CategoriesSection";
 
 const LandingPage = () => {
   return (
     <main>
       <HeroSection />
       <FeaturedProducts />
+      <CategoriesSection />
       <Testimonials />
     </main>
   );
