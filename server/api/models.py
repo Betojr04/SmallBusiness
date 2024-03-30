5bab10459d60df270eb9750a152d12cf8342cfb5
Betojr04 <betomasia12@gmail.com>
2024-03-30 12:20:50 -0700

    added product data mode title, price, description, image and category since I will be adding the cart functionality as well I need all the data models for the products and categories

--- server/api/models.py
+++ server/api/models.py
@@ -19,3 +19,26 @@ class User(db.Model):
             "username": self.username,
             "email": self.email
         }
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    
+    def __repr__(self):
+        return '<Product %r>' % self.title
+    
+    def serialize(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "price": self.price,
+            "description": self.description,
+            "category": self.category,
+            "image": self.image
+        }
+    
+class
